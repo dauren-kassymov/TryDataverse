a2168b55aea879004c90ec1401acf300a8c6dc3e
daurenK <dauren.kass@gmail.com>
2022-05-16 00:44:43 +0600

    feat: setup DI

---
+++ src/Dnka.CreateTimeEntry/IDataverseService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dnka.CreateTimeEntry
+{
+    public interface IDataverseService
+    {
+
+    }
+
+    public class DataverseService : IDataverseService
+    {
+
+    }
+}
